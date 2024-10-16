677c9cdfe550088830b78c876e9f9099d043d4f7
winnie0920 <winnie0920@hpicorp.com.tw>
2024-10-16 17:04:07 +0800

    feat: 新增滾動軸json
    1. 新增判斷

--- src/json/UserHome.js
+++ src/json/UserHome.js
@@ -21,3 +21,109 @@ export const homeLanguage = [
     name: "CH",
   },
 ];
+
+export const homeItem = [
+  {
+    id: 1,
+    name: "輕食",
+    path: "Food__Pescatora.png",
+    count: 10,
+  },
+  {
+    id: 2,
+    name: "麵包",
+    path: "Food_Syokupan.png",
+    count: 9,
+  },
+  {
+    id: 4,
+    name: "炸物、三明治",
+    path: "Food_Ebi_Fry.png",
+    count: 10,
+  },
+  {
+    id: 5,
+    name: "甜點",
+    path: "Food_Tarte.png",
+    count: 5,
+  },
+  {
+    id: 6,
+    name: "純茶",
+    path: "Food_Tea.png",
+    count: 5,
+  },
+  {
+    id: 7,
+    name: "拿鐵",
+    path: "Food_Cafe.png",
+    count: 5,
+  },
+  {
+    id: 8,
+    name: "那堤",
+    path: "Food_Milk.png",
+    count: 5,
+  },
+  {
+    id: 9,
+    name: "冰沙",
+    path: "Food_Kakigoori.png",
+    count: 5,
+  },
+];
+
+export const homeMenu = [
+  {
+    id: 1,
+    name: "輕食",
+    content: "肉品源產地：台灣雞肉、豬肉、牛肉",
+    children: [
+      {
+        id: 1,
+        name: "波隆那肉醬義大利麵",
+        dollar: "$140",
+        remark: "台灣豬肉",
+      },
+      {
+        id: 2,
+        name: "茄汁嫩雞義大利麵",
+        dollar: "$160",
+        remark: "台灣雞肉",
+      },
+      {
+        id: 3,
+        name: "茄汁季節時蔬義大利麵",
+        dollar: "$160",
+        remark: "台灣牛肉",
+      },
+      {
+        id: 4,
+        name: "茄汁牛肉義大利麵",
+        dollar: "$170",
+      },
+      {
+        id: 5,
+        name: "茄汁蛤蠣義大利麵",
+        dollar: "$180",
+      },
+      {
+        id: 6,
+        name: "蒜味嫩雞胸義大利麵",
+        dollar: "$150",
+        remark: "台灣雞肉",
+      },
+      {
+        id: 7,
+        name: "蒜味季節時蔬義大利麵",
+        dollar: "$150",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "三明治",
+    path: "path/to/image2.jpg",
+    content: "三明治的詳細信息",
+  },
+];
--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { homeItem, homeMenu } from "@/json/UserHome";
+
 const queryParams = reactive({
   search: "",
 });
@@ -8,22 +10,26 @@ const sendSearch = (val) => {
   queryParams.search = val;
 };
 
-const LabelRef = ref(null);
+const labelRef = ref(null);
+
 // 當前滾動最大寬度
 const maxScroll = ref(0);
-//滾輪假資料
-const items = [
-  {
-    id: 1,
-    name: "義大利麵",
-    path: "Food__Pescatora.png",
-  },
-];
+
+// 預設選中的菜單項目
+const selectMenu = ref(homeMenu[0]);
+
+//箭頭顯示
+const showArrow = ref(false);
+
+//圖片路徑
+const getImageUrl = (id) => {
+  return new URL(`../../assets/image/${id}`, import.meta.url).href;
+};
 
 // side左右移動
-const ScrollArrow = (direction) => {
-  if (LabelRef.value) {
-    const scrollContainer = LabelRef.value;
+const scrollArrow = (direction) => {
+  if (labelRef.value) {
+    const scrollContainer = labelRef.value;
     const currentScroll = scrollContainer.scrollLeft;
     maxScroll.value = scrollContainer.scrollWidth - scrollContainer.clientWidth;
     // 滾動值
@@ -43,19 +49,14 @@ const ScrollArrow = (direction) => {
   }
 };
 
-const ShowArrow = () => {
-  return this.items.length > 5;
-};
-
-const ClickScroll = (index, items) => {
-  if (LabelRef.value) {
-    const liWidth = LabelRef.value.querySelector("li").offsetWidth; // 每個 li 的寬度
-    const visibleWidth = LabelRef.value.clientWidth; // 可見的寬度
-    let newPosition =
-      (liWidth + 8) * index - visibleWidth / 2 + liWidth / 2 + 16; // 計算新的滾動位置
+const clickScroll = (id, items) => {
+  if (labelRef.value) {
+    const liWidth = labelRef.value.querySelector("li").offsetWidth; // 每個 li 的寬度
+    const visibleWidth = labelRef.value.clientWidth; // 可見的寬度
+    let newPosition = (liWidth + 8) * id - visibleWidth / 2 + liWidth / 2 + 16; // 計算新的滾動位置
 
     // 確保最後幾個元素正確處理
-    if (index >= items.length - 2) {
+    if (id >= items.length - 2) {
       newPosition = maxScroll.value;
     }
 
@@ -66,12 +67,31 @@ const ClickScroll = (index, items) => {
     );
 
     // 滾動到新位置
-    ScrollArrow(adjustedPosition);
+    scrollArrow(adjustedPosition);
+    //找到homeMenu中id對應的項目
+    selectMenu.value = homeMenu.find((item) => item.id === id);
+    console.log(selectMenu.value);
   }
 };
-const getImageUrl = (id) => {
-  return new URL(`../../assets/image/${id}`, import.meta.url).href;
+
+//判斷箭頭顯示
+const checkScrollWidth = () => {
+  const label = labelRef.value;
+  if (label.scrollWidth > label.clientWidth) {
+    showArrow.value = true;
+  } else {
+    showArrow.value = false;
+  }
 };
+
+//檢查箭頭是否顯示
+onMounted(() => {
+  window.addEventListener("resize", checkScrollWidth);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", checkScrollWidth);
+});
 </script>
 
 <template>
@@ -87,24 +107,28 @@ const getImageUrl = (id) => {
       <div class="col-12 col-lg-8">
         <div class="User__side sticky-top">
           <button
-            v-if="ShowArrow"
+            v-if="showArrow"
             class="User__side-left"
-            @click="ScrollArrow('left')"
+            @click="scrollArrow('left')"
           >
             <SvgIcon icon-name="Common-Arrow-Circle"></SvgIcon>
           </button>
           <button
-            v-if="ShowArrow"
+            v-if="showArrow"
             class="User__side-right"
-            @click="ScrollArrow('right')"
+            @click="scrollArrow('right')"
           >
             <SvgIcon icon-name="Common-Arrow-Circle"></SvgIcon>
           </button>
-          <ul class="User__side-label" ref="LabelRef">
+          <ul
+            :class="{ 'User__side-label-margin': showArrow }"
+            class="User__side-label"
+            ref="labelRef"
+          >
             <li
-              v-for="i in items"
+              v-for="i in homeItem"
               :key="i.id"
-              @click="ClickScroll(i.id, items)"
+              @click="clickScroll(i.id, homeItem)"
             >
               <img :src="getImageUrl(i.path)" alt="" />
               <h5>{{ i.name }} <span>(10)</span></h5>
@@ -126,93 +150,4 @@ const getImageUrl = (id) => {
 
 <style lang="scss" scoped>
 @import "@/assets/css/mixin";
-
-.User {
-  &__container {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    padding: 3rem 3rem 0;
-    gap: 1rem;
-    overflow-x: hidden;
-  }
-
-  &__side {
-    top: 8.5rem;
-    z-index: 1;
-    &-label {
-      display: flex;
-      gap: 0.5rem;
-      margin: 0 3rem;
-      padding: 0.5rem 1rem;
-      overflow-x: auto;
-      scrollbar-width: none;
-      li {
-        @extend %base-btn-setting;
-        flex-direction: column;
-        align-items: center;
-        padding: 0.8rem;
-        color: var(--cafe-color-brown-lighter);
-        background-color: var(--cafe-color-transparent);
-        cursor: pointer;
-        &:active,
-        &:hover {
-          color: var(--cafe-color-brown);
-          border: 0.1rem solid var(--cafe-color-brown);
-          background-color: var(--cafe-color-white);
-        }
-        img {
-          max-width: 100%; /* 確保圖片不會超出父容器 */
-          height: 4rem; /* 維持圖片比例 */
-          margin-bottom: 0.5rem;
-        }
-        h5 {
-          font-size: var(--cafe--font-xs);
-          white-space: nowrap;
-        }
-      }
-    }
-    &-left,
-    &-right {
-      @extend %base-btn-setting;
-      position: absolute;
-      top: 50%;
-      padding: 0.7rem;
-      border: 0.1rem solid var(--cafe-color-brown);
-      background-color: var(--cafe-color-white);
-      color: var(--cafe-color-brown);
-      transform: translateY(-50%);
-      cursor: pointer;
-      z-index: 10;
-      &:hover {
-        color: var(--cafe-color-white);
-        background-color: var(--cafe-color-brown);
-      }
-      svg {
-        width: 1rem;
-        height: 1rem;
-      }
-    }
-
-    &-left {
-      transform: rotate(180deg) translateY(50%);
-      left: 0;
-    }
-
-    &-right {
-      right: 0;
-    }
-  }
-}
-.menu {
-  &__content {
-    overflow-y: auto;
-    max-width: 100%;
-    max-height: calc(100vh - 20rem);
-    scrollbar-width: none;
-    &::-webkit-scrollbar {
-      width: 0; /* 隱藏滾動條寬度 */
-    }
-  }
-}
 </style>
